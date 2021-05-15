b1063575fd7e612ad980c1760047a18a97369dd6
mastermindvrtx <mastermindvrtx@gmail.com>
2021-05-15 23:27:41 +0845

    added group support

--- vrtxbot.py
+++ vrtxbot.py
@@ -25,16 +25,41 @@ def start(message):
 
 𝕭𝖔𝖙𝖘 ---:         
         🔴YouTube Downloader🎧📥
-         @vrtxytvrtx 
+         @vrtxytbot
         
         🟢Spotify Downloader🎤📥
-         @vrtxspotify_vrtx"""
+         @vrtxspotify_bot"""
         vrtx.reply_to(message, welcomet)  
         vrtx.reply_to(message, """
 ˜”*°•.˜”*°• I Will only echo ur msg so don't type anything else.
 Or u may try to type and check what happens •°*”˜.•°*”˜"""
                     )    
-'''----------------------------------------------------------------------'''        
+'''----------------------------------------------------------------------'''     
+@vrtx.message_handler(commands=["vrtx"])
+def vrtx(message):
+        welcomet = """
+            ℍ𝕚 & \n
+нєяє αяє αℓℓ тнє ℓιηкѕ тσ му ωσяк
+        
+𝓖𝖗𝖔𝖚𝖕 --:
+        🎻ʍʊֆɨƈǟʟ ƈɦǟttɨռɢ🎻
+         @vrtxmusic
+         
+𝕮𝖍𝖆𝖓𝖓𝖊𝖑 --:
+        🌾|••Wǟʟʟɦʊɮ-ʋʀȶӼ••|🍁
+         @vrtxwalls
+         
+        ••🔷| Vɾƚx--W๏гкร |🔶••
+         @vrtxwork 
+
+𝕭𝖔𝖙𝖘 ---:         
+        🔴YouTube Downloader🎧📥
+         @vrtxytbot
+        
+        🟢Spotify Downloader🎤📥
+         @vrtxspotify_bot"""
+        vrtx.reply_to(message, welcomet)  
+'''----------------------------------------------------------------------'''  
 @vrtx.message_handler(commands=["owner"])
 def owner(message):
         met = "@mastermindvrtx A.K.A 🃏 ʍǟֆȶɛʀʍɨռɖ.ʋʀȶӼ™ "
